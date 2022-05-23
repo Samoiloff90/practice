98d87c406d9beef2d64a8796a3ffd23a4c00fcc2
Eugene Samoilov <e.e.samoilov@mail.ru>
2022-05-23 21:39:15 +0300

    finished project

--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -1,11 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Reader {
     private String name;
     private int libraryCardNumber;
     private String faculty;
-    private int birthday;
-    private int phone;
+    private String birthday;
+    private String phone;
 
-    public Reader(String name, int libraryCardNumber, String faculty, int birthday, int phone) {
+    public Reader(String name, int libraryCardNumber, String faculty, String birthday, String phone) {
         this.name = name;
         this.libraryCardNumber = libraryCardNumber;
         this.faculty = faculty;
@@ -13,49 +16,47 @@ public class Reader {
         this.phone = phone;
     }
 
-    public String getName() {
-        return name;
+    public void takeBook(int books) {
+        System.out.println(name + " взял " + books + " книги.");
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void takeBook(String ... nameBook) {
+        String books = "";
+
+        for (int i = 0; i < nameBook.length; i++) {
+            if (i != nameBook.length - 1) {
+                books += nameBook[i] + ", ";
+            } else {
+                books += nameBook[i] + ".";
+            }
+        }
+        System.out.println(name + " взял книги: " + books);
     }
 
-    public int getLibraryCardNumber() {
-        return libraryCardNumber;
+    public void takeBook(Book ... bookObjectName) {
+        String books = "";
+        int counter = 0;
+
+        for (Book b : bookObjectName) {
+            if (counter != bookObjectName.length - 1) {
+                books += b.getName() + ", ";
+                counter++;
+            } else {
+                books += b.getName() + ".";
+            }
+        }
+        System.out.println(name +  " взял книги: " + books);
     }
 
-    public void setLibraryCardNumber(int libraryCardNumber) {
-        this.libraryCardNumber = libraryCardNumber;
-    }
+//    public void takeBook(Book ... bookObjectAll) {
+//        List<Book> booksList = new ArrayList<>();
+//
+//        for (Book b : bookObjectAll) {
+//            booksList.add(b);
+//        }
+//        System.out.println(name + "взял книги: " + booksList);
+//    }
 
-    public String getFaculty() {
-        return faculty;
-    }
-
-    public void setFaculty(String faculty) {
-        this.faculty = faculty;
-    }
-
-    public int getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(int birthday) {
-        this.birthday = birthday;
-    }
-
-    public int getPhone() {
-        return phone;
-    }
-
-    public void setPhone(int phone) {
-        this.phone = phone;
-    }
-
-    public void takeBook() {
-
-    }
     public void returnBook() {
 
     }
